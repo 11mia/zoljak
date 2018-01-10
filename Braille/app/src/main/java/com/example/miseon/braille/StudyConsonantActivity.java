c7f0e30f42f8d095dca6f141cd3cc7c161b04c17
11mia <kangms6828@naver.com>
2018-01-11 00:21:47 +0900

    consonant 버튼 수정

--- Braille/app/src/main/java/com/example/miseon/braille/StudyConsonantActivity.java
+++ Braille/app/src/main/java/com/example/miseon/braille/StudyConsonantActivity.java
@@ -4,6 +4,9 @@ package com.example.miseon.braille;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,9 +16,10 @@ public class StudyConsonantActivity extends AppCompatActivity {
     FragmentManager manager;
     FragmentTransaction tran;
     Fragment frag1,frag2,frag3;
+    Button button1;
+    Button button2;
+    Button button3;
 
-    Button button_before = null;
-    int id_before=-1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,10 @@ public class StudyConsonantActivity extends AppCompatActivity {
         frag1 = new Fragment_Initial_sound();
         frag2 = new Fragment_Ssang();
         frag3 = new Fragment_Final_consonant();
+        button1=(Button)findViewById(R.id.initial);
+        button2=(Button)findViewById(R.id.finalconsonant);
+        button3=(Button)findViewById(R.id.ssang);
+
 
 
     }
@@ -34,18 +42,51 @@ public class StudyConsonantActivity extends AppCompatActivity {
     }
 
     public void consonantClick(View v){
-        switch(v.getId()){
-            case R.id.initial:
+
+        int id = v.getId(); //클릭한 버튼
+        Resources res = getResources();
+        Drawable drawable1=res.getDrawable(R.drawable.shape_button);
+        Drawable drawable2 = res.getDrawable(R.drawable.shape_list);
+
+
+        switch(id){
+            case R.id.initial://button1
+                button1.setBackground(drawable2);
+                button1.setTextColor(Color.WHITE);
+                button2.setBackground(drawable1);
+                button2.setTextColor(Color.BLACK);
+
+                button3.setBackground(drawable1);
+                button3.setTextColor(Color.BLACK);
+
                 tran = manager.beginTransaction();
                 tran.replace(R.id.consonantFragment,frag1);
                 tran.commit();
                 break;
-            case R.id.ssang:
+            case R.id.ssang://button3
+                button3.setBackground(drawable2);
+                button3.setTextColor(Color.WHITE);
+
+                button1.setBackground(drawable1);
+                button1.setTextColor(Color.BLACK);
+
+                button2.setBackground(drawable1);
+                button2.setTextColor(Color.BLACK);
+
                 tran=manager.beginTransaction();
                 tran.replace(R.id.consonantFragment,frag2);
                 tran.commit();
                 break;
-            case R.id.finalconsonant:
+            case R.id.finalconsonant://button2
+                button2.setBackground(drawable2);
+                button2.setTextColor(Color.WHITE);
+
+                button1.setBackground(drawable1);
+                button1.setTextColor(Color.BLACK);
+
+                button3.setBackground(drawable1);
+                button3.setTextColor(Color.BLACK);
+
                 tran=manager.beginTransaction();
                 tran.replace(R.id.consonantFragment,frag3);
                 tran.commit();
@@ -54,22 +95,6 @@ public class StudyConsonantActivity extends AppCompatActivity {
         }
 
     }
-    public void click(View v){
-      /*
-        int id = v.getId();
-        Button button = (Button)findViewById(id);
-        Resources res = getResources();
-        if(id_before!=-1){
-            button_before=(Button)findViewById(id_before);
-            Drawable drawable = res.getDrawable(R.drawable.shape_button);
-            button_before.setBackground(drawable);
-        }
-        if(id!=id_before){
-            Drawable drawable=res.getDrawable(R.drawable.shape_list);
-            button.setBackground(drawable);
-            id_before=id;
-        }
-        */
-
+    public void click(View v) {
     }
 }
