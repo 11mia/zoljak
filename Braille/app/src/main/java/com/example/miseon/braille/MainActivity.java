52ce0addcd2931143490de7ce2a8e73f31cabe55
11mia <kangms6828@naver.com>
2018-01-30 19:09:38 +0900

    add action_menu

--- Braille/app/src/main/java/com/example/miseon/braille/MainActivity.java
+++ Braille/app/src/main/java/com/example/miseon/braille/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startActivity(new Intent(this, Splash_Activity.class));
+
 
         dbmanager=new DBManager(this);
         sqlitedb = dbmanager.getReadableDatabase();
@@ -48,6 +51,23 @@ public class MainActivity extends AppCompatActivity {
                 finish();
             }
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id==R.id.action_settings){
+            Intent it = new Intent(this,DeveloperActivity.class);
+            startActivity(it);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 
@@ -175,10 +195,7 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void ShowDeveloper(View v){
-        Intent it = new Intent(this,DeveloperActivity.class);
-        startActivity(it);
 
-    }
+
 }
 
