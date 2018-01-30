2bf00edb368715928c6cfad00e3802e3502c60b9
KMY0510 <vdsymk9124@gmail.com>
2018-01-30 10:59:55 +0900

    DBManager파일 작성

--- Braille/app/src/main/java/com/example/miseon/braille/DBManager.java
+++ Braille/app/src/main/java/com/example/miseon/braille/DBManager.java
@@ -22,49 +22,72 @@ public class DBManager extends SQLiteOpenHelper {
         try{
             int count=1;
             String[] letter1={"ㄱ","ㄴ","ㄷ","ㄹ","ㅁ","ㅂ","ㅅ","ㅇ","ㅈ","ㅊ","ㅋ","ㅌ","ㅍ","ㅎ"};
-            String[] initial={"in0","in1","in2","in3","in4","in5","in6","in7","in8","in9","in10","in11","in12","in13"};
-            String[] fin={"fi0","fi1","fi2","fi3","fi4","fi5","fi6","fi7","fi8","fi9","fi10","fi11","fi12","fi13"};
+            String[] initial={"c777477","c177477","c727477","c777757","c177757","c777457","c777776","c127457","c777476","c777756","c127477","c127757","c177457","c727457"};
+            String[] fin={"c177777","c727757","c773757","c727777","c727776","c127777","c773777","c723756","c173777","c723777","c723757","c723776","c727756","c773756"};
+
 
             String[] letter2={"ㄲ","ㄸ","ㅃ","ㅆ","ㅉ"};
-            String[] ssang_in={"in0","in2","in5","in6","in8"};
-            String ssang="ssang";
+            String[] ssang_in={"c777477","c727477","c777457","c777776","c777476"};
+
+
 
             String[] letter3={"ㅏ","ㅑ","ㅓ","ㅕ","ㅗ","ㅛ","ㅜ","ㅠ","ㅡ","ㅣ","ㅐ","ㅔ","ㅖ","ㅘ","ㅚ","ㅝ","ㅢ","ㅒ","ㅙ","ㅞ","ㅟ"};
-            String[] vowel={"vo0","vo1","vo2","vo3","vo4","vo5","vo6","vo7","vo8","vo9","vo10","vo11","vo12","vo13","vo14","vo15","vo16","vo17","vo18","vo19","vo20","vo21","vo22","vo23","vo24"};
+            String[] vowel={"c127776","c773457","c723477","c177756","c173776","c773476","c173477","c177476","c727476","c173757","c123757","c173457","c773477","c123776","c173456","c123477","c727456","c773457","c123757","c123776","c123757","c123477","c123757","c173477","c123757"};
 
-            String string="@string/";
-            String pic1;
-            String pic2;
+
+            String[] letter4={"A","B","C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"};
+            String[] letter4_1={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+            String[] alpha_fir={"c777776"};
+            String[] alpha={"c177777", "c127777","c177477","c177457","c177757","c127477","c127457","c127757","c727477","c727457","c173777","c123777","c173477"
+            ,"c173457","c173757","c123477","c123457","c123757","c723477","c723457","c173776","c123776","c727456","c173476","c173456","c173756"};
+
+
+
+            String[] letter5={"0","1","2","3","4","5","6","7","8","9"};
+            String[] num_fir={"c773456"};
+            String[] num={"c727457","c177777","c127777","c177477","c177457","c177757","c127477","c127457","c127757","c727477"};
+
+
+
+            String[] letter6={".", "?", "!", ",", "“", "”", "’"};
+            String[] symbol={"c727756","c723776","c723757","c777757","c723776","c773756","c773777"};
+
+            String[] letter6_2={"·", ":",";","/","‘","’","(",")","{","}","[","]","~","*","〃","￦","￠","$","￡","￥","€"};
+            String[] symbol_2_front={"c777757","c777757","c777756","c777456","c777776","c773756","c723776","c777776","c723776","c777757","c723776","c777756",
+                    "c773776","c773757","c777756","c777477","c777477","c777477","c777477","c777477","c777477"};
+            String[] symbol_2_back={"c723777","c727777","c723777","c773477","c723776","c773777","c773777","c773756","c727777","c773756",
+            "c723777","c773756","c773776","c773757","c723777","c727456","c177477","c177457","c123777","c173456","c177757"};
+
+            String[] letter6_3={"······","......"};
+            String[] symbol_3={"c777776","c727756"};
+
+
             long newRowId;
             for(int i=0;i<14;i++) {
-                pic1 = string+initial[i];
                 values.put("num", count++);
                 values.put("letter", letter1[i]);
                 values.put("type", "초성");
                 values.put("dot_num",1);
-                values.put("dot_1",pic1);
+                values.put("dot_1",initial[i]);
                 db.insert("Braille",null,values);
             }
 
             for(int i=0;i<14;i++) {
-                pic1 = string+fin[i];
                 values.put("num", count++);
                 values.put("letter", letter1[i]);
                 values.put("type", "종성");
                 values.put("dot_num",1);
-                values.put("dot_1",pic1);
+                values.put("dot_1",fin[i]);
                 db.insert("Braille",null,values);
             }
 
             for(int i=0;i<5;i++) {
-                pic1 = string+ssang;
-                pic2 = string+ssang_in[i];
                 values.put("num", count++);
                 values.put("letter", letter2[i]);
                 values.put("type", "된소리초성");
                 values.put("dot_num",2);
-                values.put("dot_1",pic1);
-                values.put("dot_2",pic2);
+                values.put("dot_1","c777776");
+                values.put("dot_2",ssang_in[i]);
                 db.insert("Braille",null,values);
             }
 
@@ -72,49 +95,115 @@ public class DBManager extends SQLiteOpenHelper {
             values.put("letter","ㄲ");
             values.put("type","된소리종성");
             values.put("dot_num",2);
-            values.put("dot_1","@string/ssang_fi0");
-            values.put("dot_2","@string/ssang_fi0");
+            values.put("dot_1","c177777");
+            values.put("dot_2","c177777");
             db.insert("Braille",null,values);
-            values.put("dot_2", (String) null);
 
 
             values.put("num",count++);
             values.put("letter","ㅆ");
             values.put("type","된소리종성");
             values.put("dot_num",1);
-            values.put("dot_1","@string/ssang_fi1");
-            db.insert("Braille",null,values);
+            values.put("dot_1","c773477");
             values.put("dot_2", (String) null);
+            db.insert("Braille",null,values);
 
 
 
             for(int i=0;i<17;i++) {
-                pic1 = string+vowel[i];
                 values.put("num", count++);
                 values.put("letter", letter3[i]);
                 values.put("type", "중성");
                 values.put("dot_num",1);
-                values.put("dot_1",pic1);
+                values.put("dot_1",vowel[i]);
                 db.insert("Braille",null,values);
             }
-            for(int i=17,j=17;i<25;i=i+2,j++) {
 
-                pic1 = string+vowel[i];
-                pic2 = string+vowel[i+1];
+            for(int i=17,j=17;i<25;i=i+2,j++) {
                 values.put("num", count++);
                 values.put("letter", letter3[j]);
                 values.put("type", "중성");
                 values.put("dot_num",2);
-                values.put("dot_1",pic1);
-                values.put("dot_2",pic2);
+                values.put("dot_1", vowel[i]);
+                values.put("dot_2",vowel[i+1]);
                 db.insert("Braille",null,values);
             }
 
+            for(int i=0; i<26; i++) {
+                values.put("num", count++);
+                values.put("letter", letter4[i]);
+                values.put("type", "알파벳대문자");
+                values.put("dot_num", 2);
+                values.put("dot_1", alpha_fir[0]);
+                values.put("dot_2",alpha[i]);
+                db.insert("Braille", null, values);
+            }
 
+            for(int i=0; i<26; i++) {
+                values.put("num", count++);
+                values.put("letter", letter4_1[i]);
+                values.put("type", "알파벳소문자");
+                values.put("dot_num", 1);
+                values.put("dot_1",alpha[i]);
+                values.put("dot_2", (String) null);
+                db.insert("Braille", null, values);
+            }
+
+            for (int i=0; i<10; i++ ) {
+                values.put("num", count++);
+                values.put("letter",letter5[i]);
+                values.put("type","숫자");
+                values.put("dot_num",2);
+                values.put("dot_1", num_fir[0]);
+                values.put("dot_2",num[i]);
+                db.insert("Braille", null, values);
+            }
+
+            values.put("num", count++);
+            values.put("letter","10");
+            values.put("type", "숫자");
+            values.put("dot_num",3);
+            values.put("dot_1", num_fir[0]);
+            values.put("dot_2", num[1]);
+            values.put("dot_3", num[0]);
+            db.insert("Braille", null, values);
+
+
+            for(int i=0; i<7; i++) {
+                values.put("num", count++);
+                values.put("letter",letter6[i]);
+                values.put("type","문장부호");
+                values.put("dot_num",1);
+                values.put("dot_1", symbol[i]);
+                values.put("dot_2", (String) null);
+                values.put("dot_3", (String) null);
+                db.insert("Braille", null, values);
+            }
+
+            for (int i=0; i<21; i++) {
+                values.put("num", count++);
+                values.put("letter",letter6_2[i]);
+                values.put("type","문장부호");
+                values.put("dot_num",2);
+                values.put("dot_1", symbol_2_front[i]);
+                values.put("dot_2", symbol_2_back[i]);
+                values.put("dot_3", (String) null);
+                db.insert("Braille", null, values);
+            }
+
+
+           for(int i=0; i<2; i++) {
+               values.put("num", count++);
+               values.put("letter", letter6_3[i]);
+               values.put("type", "문장부호");
+               values.put("dot_num", 3);
+               values.put("dot_1", symbol_3[i]);
+               values.put("dot_2", symbol_3[i]);
+               values.put("dot_3", symbol_3[i]);
+               db.insert("Braille", null, values);
+           }
 
             db.setTransactionSuccessful();
-
-
 
         }catch(SQLiteException e){
 
@@ -122,8 +211,6 @@ public class DBManager extends SQLiteOpenHelper {
             db.endTransaction();
         }
     }
-
-
 
 
     @Override
